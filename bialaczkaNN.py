69397fa7e108cc46e3aaa8d6426d90698e41c5ba
Kuba Sanecki <qba.qxy@gmail.com>
2017-11-05 17:18:22 +0100

    Added files with data in csv, loading of training data

---
+++ bialaczkaNN.py
@@ -0,0 +1,12 @@
+import numpy as np
+from numpy import genfromtxt
+# Input Array
+# Output Array
+training_path = '/Users/jakubsanecki/Desktop/bialaczka/Bialaczki_dane/'
+my_data = genfromtxt(training_path + 'training_l2type.csv', delimiter=';')
+# lines = open(training_path + 'training_bazofilowa.csv', "r").readlines()
+# print(lines)
+# Sigmoid Function
+print(my_data)
+# print(my_data[0])
+# Sigmoid Function Derivative
