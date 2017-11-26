ad0b5dabc869dc95cdc1910958b59e0c99935f01
Kuba Sanecki <qba.qxy@gmail.com>
2017-11-26 15:54:11 +0100

    Added CSVs definitions

--- bialaczkaNN.py
+++ bialaczkaNN.py
@@ -3,10 +3,34 @@ from numpy import genfromtxt
 # Input Array
 # Output Array
 training_path = '/Users/jakubsanecki/Desktop/bialaczka/Bialaczki_dane/'
-my_data = genfromtxt(training_path + 'training_l2type.csv', delimiter=';')
+# Defining CSVs
+bazofilowa = genfromtxt(training_path + 'training_bazofilowa.csv', delimiter=';')
+bialaczka_komorek_wielkojadrzastych = genfromtxt(training_path + 'training_bialaczka_komorek_wielkojadrzastych.csv', delimiter=';')
+chloniak_limfatyczny = genfromtxt(training_path + 'training_chloniak_limfatyczny.csv', delimiter=';')
+cytolukemia = genfromtxt(training_path + 'training_cytolukemia.csv', delimiter=';')
+differentiation_in_part = genfromtxt(training_path + 'training_differentiation_in_part.csv', delimiter=';')
+differentiation = genfromtxt(training_path + 'training_differentiation.csv', delimiter=';')
+eozyrofilowa = genfromtxt(training_path + 'training_eozyrofilowa.csv', delimiter=';')
+granulamononuclear = genfromtxt(training_path + 'training_granulamononuclear.csv', delimiter=';')
+granulocylosis = genfromtxt(training_path + 'training_granulocylosis.csv', delimiter=';')
+granulocyte = genfromtxt(training_path + 'training_granulocyte.csv', delimiter=';')
+l2type = genfromtxt(training_path + 'training_l2type.csv', delimiter=';')
+ltype = genfromtxt(training_path + 'training_ltype.csv', delimiter=';')
+lymphocytic = genfromtxt(training_path + 'training_lymphocytic.csv', delimiter=';')
+mielomonocytarna = genfromtxt(training_path + 'training_mielomonocytarna.csv', delimiter=';')
+mlemonoblastyczna = genfromtxt(training_path + 'training_mlemonoblastyczna.csv', delimiter=';')
+monoblastyczna = genfromtxt(training_path + 'training_monoblastyczna.csv', delimiter=';')
+monocytarna = genfromtxt(training_path + 'training_monocytarna.csv', delimiter=';')
+plazmocytowa = genfromtxt(training_path + 'training_plazmocytowa.csv', delimiter=';')
+subacute_granulocyte = genfromtxt(training_path + 'training_subacute_granulocyte.csv', delimiter=';')
+wielokapilarnokomorkowa = genfromtxt(training_path + 'training_wielokapilarnokomorkowa.csv', delimiter=';')
+
+
 # lines = open(training_path + 'training_bazofilowa.csv', "r").readlines()
 # print(lines)
 # Sigmoid Function
-print(my_data[0][1])
+
+
+print(my_data)
 # print(my_data[0])
 # Sigmoid Function Derivative
